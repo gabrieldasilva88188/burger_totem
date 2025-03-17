f1adbb1ff1c80059d85272666423227b60422d63
Felipe Ferreira De Souza <felipefrs2007@gmail.com>
2025-03-16 23:13:19 -0300

    Integração com as tela 0 e tela 1

--- TotemPWA/Controllers/HomeController.cs
+++ TotemPWA/Controllers/HomeController.cs
@@ -17,6 +17,11 @@ public class HomeController : Controller
     {
         return View();
     }
+
+     public IActionResult cardapio()
+    {
+        return View();
+    }
        
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]

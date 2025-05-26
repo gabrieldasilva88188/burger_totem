4fccb83bd45f3706b67a0877f21dcbefd7b5c0a9
mariajloureiro <mariajloureiro06@gmail.com>
2025-05-26 17:50:40 -0300

    Adicionando a tela loading

--- TotemPWA/Controllers/HomeController.cs
+++ TotemPWA/Controllers/HomeController.cs
@@ -122,10 +122,14 @@ public class HomeController : Controller
     {
         return View();
     }
-
+    public IActionResult Carregamento()
+    {
+        return View();
+    }
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+    
 }
